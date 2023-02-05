649e81c1f96b6d9ff13f4a927fa1d1f1d799eadc
KendyKim <86278572+chlorinebot@users.noreply.github.com>
2023-02-05 19:23:51 +0700

    Update CONFIG.js

--- LOVE/CONFIG.js
+++ LOVE/CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
-  titleHeader: "EM CÓ YÊU ANH NHIỀU KHÔNG BẢO THUẬN 💞",
+  titleHeader: "EM CÓ YÊU ANH NHIỀU KHÔNG BÉ YÊU 💞",
   descriptionHeader:
-    "Nếu em đóng tab là hết yêu anh rồi :(",
+    "Nếu em đóng tab là hết yêu anh rồi :(((",
   buttonYes: "Nhiều chứ!",
   buttonNo: "Không có nhiều.",
-  titleModar: "EM YÊU CỤC CƯNG EM NHẤT TRÊN ĐỜIIIIIII",
+  titleModar: "ANH YÊU EM NHẤT TRÊN ĐỜIIIIIII",
   descriptionModar:
-    'BIẾT KIỂU GÌ CỤC CƯNG CŨNG KHÔNG THỂ CƯỠNG LẠI ĐƯỢC EM ĐÂU AHIHI',
+    'BIẾT KIỂU GÌ EM YÊU CŨNG KHÔNG THỂ CƯỠNG LẠI ĐƯỢC ANH ĐÂU AHIHI',
 };
